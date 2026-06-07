29087e7ea0d21be84cb2436217eca8945e414300
GauravTandi <148497273+GauravTandi@users.noreply.github.com>
2026-06-07 12:11:42 +0000

    feat: complete student management CLI with full CRUD and menu system

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ def add_student():
     conn.commit()
     conn.close()
 
+
 def view_students():
     conn = get_connection()
     cursor = conn.cursor()
@@ -36,3 +37,75 @@ def view_students():
         print(f"id:{row[0]} | name:{row[1]} | age:{row[2]} | mark:{row[3]}")
 
     conn.close()
+
+
+
+def update_student():
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    student_id = int(input("Enter student id: "))
+    new_name = input("Enter the new name: ")
+    new_age = int(input("Enter new age: "))
+    new_mark = int(input("Enter new mark: "))
+
+    query = """
+        UPDATE student 
+        SET name=%s, age=%s, mark=%s
+        WHERE id=%s
+    """
+    values = (new_name, new_age, new_mark, student_id)
+
+    cursor.execute(query,values)
+    conn.commit()
+    conn.close()
+
+
+def delete_student():
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    student_id = int(input("Enter the student ID to delete: "))
+    query = """
+    DELETE FROM student
+    WHERE id=%s
+    """
+    values = (student_id),
+
+    cursor.execute(query,values)
+    conn.commit()
+
+    print("student deleted successfully")
+    conn.close()
+
+def menu():
+    while True:
+        print("\n-------STUDENT-MANAGEMENT-SYSTEM--------")
+        print("1. Add Students")
+        print("2. View Students")
+        print("3. Update Student")
+        print("4. Delete Student")
+        print("5. Exit")
+
+        choice = input("Enter choice: ")
+
+        if choice == "1":
+            add_student()
+        
+        elif choice == "2":
+            view_students()
+        
+        elif choice == "3":
+            update_student()
+        
+        elif choice == "4":
+            delete_student()
+        
+        elif choice == "5":
+            print("Exiting...")
+            break
+        
+        else:
+            print("Invaild choice")
+
+menu()
